ba2b362d2ba65ecbc8a81139a4660f0051a6871a
Fady Michel <fadymichel.r@gmail.com>
2018-02-11 17:53:46 +0100

    more performance

--- FormValidator.php
+++ FormValidator.php
@@ -8,8 +8,12 @@
 namespace Fady\Form;
 
 
+use Fady\Form\Traits\Validator;
+
 class FormValidator
 {
+
+    use Validator;
 
     const REQUIRED = 1;
     const INT = 3;
@@ -152,127 +156,6 @@ class FormValidator
         return $this;
     }
 
-    /**
-     * @param $email
-     * @return mixed
-     */
-    public function isEmail($email)
-    {
-
-        return filter_var($email, FILTER_VALIDATE_EMAIL) ? $email : false;
-    }
-
-    /**
-     * @param $value
-     * @return bool
-     */
-    public function isAlnum($value)
-    {
-        return ctype_alnum($value) ? $value : false;
-    }
-
-
-    /**
-     * @param $value
-     * @return bool
-     */
-    public function choice($value, $option)
-    {
-        return in_array($value, $option) ? $value : false;
-    }
-
-
-    /**
-     * @param $value
-     * @param null $option
-     * @return bool
-     */
-    public function validStrLen($value, $option = null)
-    {
-
-        $min = $option ? $option['min'] : null;
-        $max = $option ? $option['max'] : null;
-
-        $len = strlen($value);
-        if ($len < $min && !is_null($min)) {
-            return false;
-        } elseif ($len > $max && !is_null($max)) {
-            return false;
-        }
-        return $value;
-
-    }
-
-    /**
-     * @param $int
-     * @return bool
-     */
-    public function isInt($int, $option = null)
-    {
-
-        if (is_numeric($int)) {
-
-            $int = (int)$int;
-            return filter_var($int, FILTER_VALIDATE_INT, $option ? $options = [
-                'options' => $option
-            ] : FILTER_FLAG_ALLOW_OCTAL) ? $int : false;
-        }
-        return false;
-    }
-
-    /**
-     * @param $date
-     * @return bool|string
-     */
-    public function isDate($date, $option = null)
-    {
-        $format = $option ? $option['format'] : 'd-m-Y';
-
-        try {
-            $dateTime = new \DateTime();
-            $date = $dateTime->createFromFormat($format, $date);
-            return $date instanceof \DateTime ? $date : false;
-
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
-
-    /**
-     * @param $time
-     * @return bool|string
-     */
-    public function isTime($time, $option = null)
-    {
-        $format = $option ? $option['format'] : 'h:i';
-        try {
-            $dateTime = new \DateTime();
-            $time = $dateTime->createFromFormat($format, $time);
-            return $time instanceof \DateTime ? $time : false;
-
-        } catch (\Exception $e) {
-            return false;
-        }
-    }
-
-    /**
-     * @param $value
-     * @return bool
-     */
-    public function isRequired($value)
-    {
-
-        return !empty($value) ? $value : false;
-    }
-
-    /**
-     * @param $value
-     * @return bool
-     */
-    public function isNumeric($value)
-    {
-        return is_numeric($value);
-    }
 
     public function validateItem($value, $type, $option = null)
     {
---
+++ Traits/Validator.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fadymichel
+ * Date: 11/02/18
+ * Time: 17:39
+ */
+
+namespace Fady\Form\Traits;
+
+
+trait Validator
+{
+
+    /**
+     * @param $email
+     * @return mixed
+     */
+    public function isEmail($email)
+    {
+
+        return filter_var($email, FILTER_VALIDATE_EMAIL) ? $email : false;
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function isAlnum($value)
+    {
+        return ctype_alnum($value) ? $value : false;
+    }
+
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function choice($value, $option)
+    {
+        return in_array($value, $option) ? $value : false;
+    }
+
+
+    /**
+     * @param $value
+     * @param null $option
+     * @return bool
+     */
+    public function validStrLen($value, $option = null)
+    {
+
+        $min = $option ? $option['min'] : null;
+        $max = $option ? $option['max'] : null;
+
+        $len = strlen($value);
+        if ($len < $min && !is_null($min)) {
+            return false;
+        } elseif ($len > $max && !is_null($max)) {
+            return false;
+        }
+        return $value;
+
+    }
+
+    /**
+     * @param $int
+     * @return bool
+     */
+    public function isInt($int, $option = null)
+    {
+
+        if (is_numeric($int)) {
+
+            $int = (int)$int;
+            return filter_var($int, FILTER_VALIDATE_INT, $option ? $options = [
+                'options' => $option
+            ] : FILTER_FLAG_ALLOW_OCTAL) ? $int : false;
+        }
+        return false;
+    }
+
+    /**
+     * @param $date
+     * @return bool|string
+     */
+    public function isDate($date, $option = null)
+    {
+        $format = $option ? $option['format'] : 'd-m-Y';
+
+        return $this->createDateTime($time, $format);
+    }
+
+    /**
+     * @param $time
+     * @return bool|string
+     */
+    public function isTime($time, $option = null)
+    {
+        $format = $option ? $option['format'] : 'h:i';
+        return $this->createDateTime($time, $format);
+    }
+
+    /**
+     * @param $dateOrTime
+     * @param string $format
+     * @return bool|\DateTime
+     */
+    public function createDateTime($dateOrTime, $format = 'm-d-Y h:i') {
+        $format = $option ? $option['format'] : 'h:i';
+
+        try {
+            $dateTime = new \DateTime();
+            $dateOrTime = $dateTime->createFromFormat($format, $dateOrTime);
+            return $dateOrTime instanceof \DateTime ? $dateOrTime : false;
+
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function isRequired($value)
+    {
+
+        return !empty($value) ? $value : false;
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public function isNumeric($value)
+    {
+        return is_numeric($value);
+    }
+
+}
